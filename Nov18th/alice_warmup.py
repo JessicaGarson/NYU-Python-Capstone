ebf13fdf1ba4476cf636b12a04640da5b2d5d0e0
mishka28 <mishiko28_chigo@yahoo.com>
2017-11-18 10:46:53 -0500

    I have updated it

--- Nov18th/alice_warmup.py
+++ Nov18th/alice_warmup.py
@@ -14,3 +14,17 @@ s='make a function that removes all vowels from a sentense.'
 
 if __name__ == "__main__":
 	vowel()
+	
+# #! usr/bin/env python
+
+# def vowel(sentance):
+# 	vowels = ('a', 'e', 'i', 'o', 'u', 'y')
+# 	s1 = []
+# 	for l in s:
+# 		if l not in vowels:
+# 			s1.append(l)
+# 	return ''.join(s1)
+# if __name__ == "__main__":
+
+# 	s='make a function that removes all vowels from a sentense.'
+# 	print(vowel(s))
