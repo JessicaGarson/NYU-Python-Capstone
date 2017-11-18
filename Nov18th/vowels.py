005c4f634f6ea1241de5a458e2342877033f86f1
dgoldman916 <deanjgoldman@gmail.com>
2017-11-18 10:56:41 -0500

    Update vowels.py

--- Nov18th/vowels.py
+++ Nov18th/vowels.py
@@ -1,10 +1,11 @@
 def del_vowels(sentence):
-    sentence = list(str(sentence))
+    sentence = str(sentence)
     vowels = ['a','e','i','o','u']
+    sentence_no_vowels = sentence
     for letter in sentence:
         if letter in vowels:
-            sentence.remove(letter)
-    return ''.join(sentence)
+            sentence_no_vowels = sentence_no_vowels.replace(letter, '')
+    return sentence_no_vowels
 
 def get_sentence():
     sentence = input('Enter a sentence: ')
